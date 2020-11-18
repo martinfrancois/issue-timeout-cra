22bbcca808af75ee64876bd4955217489873f2ef
martinfrancois <f.martin@fastmail.com>
2020-11-18 12:15:49 +0100

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+describe('Timeout', () => {
+  function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  it('should not timeout', async () => {
+    jest.setTimeout(30000);
+    await sleep(10000);
+  });
 });
